71c2cee60b3d478dcb54d1a1655226f03a1323fb
ShorY <shorymarkiting@gmail.com>
2024-02-02 13:09:07 +0200

    build digital clock

--- src/Components/DigitalClock/DigitalClock.jsx
+++ src/Components/DigitalClock/DigitalClock.jsx
@@ -1,18 +1,46 @@
-import React, {useEffect,useState} from "react";
-import './DigitalClock.css'
+import React, { useEffect, useState } from "react";
+import "./DigitalClock.css";
+import reactLogo from "/src/assets/react.svg";
 
-export default function DigitalClock(){
-    const [time, setTime] = useState(new Date());
+export default function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+     document.title = "Racheli Shor"
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+        setTime(new Date(), 1000)     
+    }, 1000);
+    return () => {
+        clearInterval(intervalId);
+    }
+    }, []);
 
-    useEffect(() => {},[dependencies])
 
-    return (
-        <div className="colck-container">
-            <div className="clock">
-                <span>00:00:00</span>
-            </div>
+  function formatTime() {
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let format = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12
+    
+    return `${padTime(hours)}:${padTime(minutes)}:${padTime(seconds)}` ; 
 
-        </div>
-    );
+    function padTime(number){
+        return (number < 10 ? "0" : "") + number;
+    }
+  }
 
+
+  return (
+    <div className="colck-container">
+      <div className="clock">
+        <a className="logo">
+          <img src={reactLogo} className="logo" alt="React logo" />
+        </a>
+        <span>{formatTime()}</span>
+        <a className="logo">
+          <img src={reactLogo} className="logo" alt="React logo" />
+        </a>
+      </div>
+    </div>
+  );
 }
